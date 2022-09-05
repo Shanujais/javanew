633277e75c2d113d86ce6b818af1df45909f91be
Shanu <naughtynaruto1@gmail.com>
2022-09-06 00:33:08 +0530

    created new project

---
+++ src/Concrete.java
@@ -0,0 +1,14 @@
+
+public class Concrete extends Abstract 
+{
+	public void method1()
+	{
+		System.out.println(" i am abstract");
+	}
+   public static void main(String[]args)
+   {
+	   Concrete obj=new Concrete();
+	   obj.method1();
+   }
+
+}
